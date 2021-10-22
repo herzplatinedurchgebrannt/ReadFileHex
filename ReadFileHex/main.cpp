bfb386bddeb283491feadd052f5f305cf163e2d3
LX <alexandermathieu@googlemail.com>
2021-10-22 12:30:49 +0200

    Initial Commit

---
+++ ReadFileHex/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ReadFileHex
+//
+//  Created by Alexander Mathieu on 22.10.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
